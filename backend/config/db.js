8d1b2e0c0a1e9f3fbbc6a9b46216f9e6e44af25e
Andrii Bondar <andybondar3@gmail.com>
2025-03-31 12:58:02 +0100

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>  {
-    await mongoose.connect('mongodb+srv://flooN-:barabarabara@cluster0.wnp34xy.mongodb.net/food-delivery').then(() => console.log('DB Connected'))
-}
+export const connectDB = async () => {
+  await mongoose
+    .connect(`${process.env.MONGODB_URI}/food-delivery`)
+    .then(() => console.log("DB Connected"));
+};
